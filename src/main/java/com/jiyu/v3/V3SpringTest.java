e5745510b82187b8e4febc54402b7420a8454b1d
wangyiwen <wangyiwen@csii.com.cn>
2020-11-06 14:14:52 +0800

    使用@Import注入bean

--- src/main/java/com/jiyu/v3/V3SpringTest.java
+++ src/main/java/com/jiyu/v3/V3SpringTest.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class V3SpringTest {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(MyConfig.class);
-        Win10Entity win10Entity = (Win10Entity) annotationConfigApplicationContext.getBean("win10Entity");
+        Win10Entity win10Entity = (Win10Entity) annotationConfigApplicationContext.getBean("com.jiyu.v3.entity.Win10Entity");
         System.out.println(win10Entity);
 
     }
--- src/main/java/com/jiyu/v3/config/MyConfig.java
+++ src/main/java/com/jiyu/v3/config/MyConfig.java
@@ -4,13 +4,26 @@ import com.jiyu.v3.entity.Win10Entity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
 
 @Configuration
+@Import(Win10Entity.class)
 public class MyConfig {
 
-    @Conditional(MyCondition.class)
-    @Bean
-    public Win10Entity win10Entity(){
-        return new Win10Entity();
-    }
+    /**
+     * Import作用主要讲外部的jar包注入到springioc容器 @Import(Win10Entity.class) 等于 @Bean
+     * Import注册的bean对象 id为当前类全路径
+     */
+//    @Conditional(MyCondition.class)
+//    @Bean
+//    public Win10Entity win10Entity(){
+//        return new Win10Entity();
+//    }
+
+    /**
+     * @Import注解与@Bean注解的区别
+     * bean注解注册的bean的id是方法名称 @Import以当前类完整路径地址注册 相比来说@Import注入更加简单
+     * 共同点：都是引入外部的jar包
+     */
 }
